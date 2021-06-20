e4c3216affdf2a741753561278fc0a362ec4ee34
Maruf Hossain <hossain815265@gmail.com>
2021-06-20 11:27:25 +0600

    done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const io = new Server(server, {
 
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({origin: "*"}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('service'))
